61310daba101b57a2b7e0a71db03d380c6e762ef
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-04-04 20:51:56 +0200

    Add user channel to config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     telegram_bot_token: process.env.TELEGRAM_BOT_TOKEN,
 
     admin_chat: -1001096173716,
-    user_chat: 1,
+    user_chat: -1001082888946,
     test_chat: -210849226,
 }
